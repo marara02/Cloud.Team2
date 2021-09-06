26e31c673bd4e60c34541fd313f128e79436dd70
Marara <57908460+marara02@users.noreply.github.com>
2021-09-06 09:36:17 +0600

    [ADD] new files

---
+++ src/main/java/kz/aitu/team2/cloud/lesson/cloudlesson/CloudLessonApplication.java
@@ -0,0 +1,39 @@
+package kz.aitu.team2.cloud.lesson.cloudlesson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+
+@SpringBootApplication
+@RestController
+
+public class CloudLessonApplication {
+    private ArrayList<String> names = new ArrayList<>();
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudLessonApplication.class, args);
+    }
+
+    @GetMapping("/hello")
+    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+        this.names.add(name);
+        return String.format("Hello %s!", name);
+    }
+
+    @GetMapping("/page")
+    public String showHomePage() {
+        return "main";
+    }
+
+    @GetMapping("/list")
+    public String List_Collection() {
+        return String.format("Hello %s!", this.names.toString()
+                .replace("[", "")
+                .replace("]", "")
+        );
+    }
+}
